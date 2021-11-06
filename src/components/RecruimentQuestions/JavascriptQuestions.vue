1e490d19c7e202481cfb442890751a8a48089126
Datureli <soleris1@gmail.com>
2021-11-06 09:11:24 +0100

    add disable answer method

--- src/components/RecruimentQuestions/JavascriptQuestions.vue
+++ src/components/RecruimentQuestions/JavascriptQuestions.vue
@@ -14,12 +14,18 @@
           <v-list-item-subtitle
             class="text-h5 text-wrap"
             v-if="page === 1"
+            v-show="disable"
             v-text="javascriptItems.answer"
           ></v-list-item-subtitle>
           <v-list-item-subtitle
             class="text-h5 text-wrap"
             v-if="page === 2"
             v-text="javascriptItems.secondAnswer"
+          ></v-list-item-subtitle>
+                 <v-list-item-subtitle
+            class="text-h5 text-wrap"
+            v-if="page === 3"
+            v-text="javascriptItems.thirdPartOfAnswer"
           ></v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
@@ -28,6 +34,7 @@
         v-model="page"
         :length="3"
       ></v-pagination>
+      <v-btn @click="disableAnswers">Disable answers</v-btn>
     </v-carousel-item>
   </v-carousel>
 </template>
@@ -39,8 +46,14 @@ export default {
       type: Number,
     },
   },
+  methods: {
+    disableAnswers() {
+      this.disable = !this.disable
+    }
+  },
   data() {
     return {
+      disable: true,
       javascriptItems: [
         {
           answerNumbers: 2,
@@ -48,13 +61,16 @@ export default {
           answer:
             "Aby zadeklarować zmienną, powinniśmy posłużyć się jednym ze słów kluczowych.Do stworzenia zmiennej w JavaScript przez lata używało się słowa kluczowego var.Wraz z ewolucją javascriptu.Wraz z pojawieniem się ES6 (ECMAScript 2015) wprowadzono nowe sposoby deklaracji zmiennych za pomocą słów kluczowych let i const.",
           secondAnswer:
-            "wielu programistów zaleca stosować już const i let. Dzięki nim nasze skrypty stają się nie tylko bardziej optymalne pod względem zarządzania pamięcią, ale i potencjalnie unikamy niektórych problematycznych sytuacji",
+            "wielu programistów zaleca stosować już const i let. Dzięki nim nasze skrypty stają się nie tylko bardziej optymalne pod względem zarządzania pamięcią, ale i potencjalnie unikamy niektórych problematycznych sytuacji.Pod zmienne możemy podstawić dowolne wartości jakie będziemy chcieli",
+            thirdPartOfAnswer:  'Nazwy zmiennych i stałych które deklarujemy nie mogą być byle jakie. Istnieją pewne zasady których musimy się trzymać. I tak:wielkość liter ma znaczenie. Zmienna myTXT to nie to samo co mytxt,nazwa zmiennej nie może zaczynać się od cyfry,,nazwa zmiennej nie może zawierać spacji, kropki, przecinka ani myślnika (można natomiast używać podkreślenia),nazwą zmiennej nie może być słowo kluczowe zarezerwowane przez JavaScript'
         },
+
         {
-          answerNumbers: 1,
-          question: "Czym jest Css sprites?",
+          question: "Jaka jest różnica między var,let a const?",
           answer:
-            "To technika pozwalająca na łączenie wielu mniejszych obrazów, wykorzystywanych na stronie www, w jeden większy plik, który przy odpowiedniej pomocy styli CSS pozwoli zaoszczędzić nam pasmo serwera oraz czas ładowania się strony,",
+            "Pierwsza i najważniejsza różnica między let/const a var to zasięg zmiennych.W przypadku let/const zmienne mają zasięg blokowy, co w skrócie oznacza od klamry do klamry.Zmienne deklarowane za pomocą var mają natomiast zasięg funkcyjny, czyli ich zasięg określa ciało funkcji.",
+
+
         },
       ],
     };
