c55448719d28f89c5bc1bceaab45236dcc49273c
Datureli <soleris1@gmail.com>
2021-12-26 09:48:31 +0100

    remove unused library from main.js

--- src/main.js
+++ src/main.js
@@ -3,13 +3,11 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import store from './store/store'
-import VueHtml2Canvas from 'vue-html2canvas';
 
 
 new Vue({
   vuetify,
   router,
   store,
-  VueHtml2Canvas,
   render: h => h(App)
 }).$mount('#app')
