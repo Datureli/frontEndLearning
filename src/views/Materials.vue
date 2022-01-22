c5d171f8339b4b3e84380307f13c5749fea8ad1c
Datureli <soleris1@gmail.com>
2022-01-22 18:02:10 +0100

    adding dynamic class binding

--- src/views/Materials.vue
+++ src/views/Materials.vue
@@ -2,14 +2,20 @@
   <v-card height="540" class="triangle d-flex">
     <v-spacer></v-spacer>
 
-    <materialCard class="mt-2">
+    <materialCard
+      class="mt-2"
+      :class="{ active: isActive }"
+      @click="toggleClass('app', 'flashing')"
+    >
       <Slot title="tab1" class="tab1 mx-auto text-h5">Video</Slot>
     </materialCard>
     <materialCard class="mt-7 text-center">
       <Slot title="tab2" class="mx-auto text-h5">Książki</Slot>
     </materialCard>
     <materialCard class="mt-16">
-      <Slot title="tab3" style="word-break: break-word" class="mx-auto text-h5">Strony internetowe</Slot>
+      <Slot title="tab3" style="word-break: break-word" class="mx-auto text-h5"
+        >Strony internetowe</Slot
+      >
     </materialCard>
     <v-spacer></v-spacer>
   </v-card>
@@ -22,12 +28,16 @@ export default {
   firstColumnClicked: false,
   secondColumnClicked: false,
   ThirdColumnClicked: false,
+  isActive: false,
   components: {
     MaterialCard,
     Slot,
   },
   methods: {
-    
+    myFilter() {
+      this.isActive = !this.isActive;
+      // some code to filter users
+    },
   },
 };
 </script>
@@ -48,5 +58,20 @@ export default {
   color: rebeccapurple;
   height: 100px;
   text-align: center;
+}
+.flashing {
+  animation: flash 2.5s;
+  background: brown;
+}
+@keyframes flash {
+  0% {
+    background-color: none;
+  }
+  50% {
+    background-color: green;
+  }
+  100% {
+    background-color: none;
+  }
 }
 </style>
