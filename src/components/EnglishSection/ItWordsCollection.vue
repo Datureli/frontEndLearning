10362b043078ba8394a516095722cc6afcd7e8f4
Datureli <soleris1@gmail.com>
2022-01-23 20:03:49 +0100

    working at english word component

--- src/components/EnglishSection/ItWordsCollection.vue
+++ src/components/EnglishSection/ItWordsCollection.vue
@@ -1,27 +1,36 @@
 <template>
-<v-container>
-    <v-card width="500" height="300" class="mx-auto mt-15">
-    {{ carouselCounter }}
-    <v-carousel hide-delimiters prev-icon @change="incrementCarouselCounter">
-      <v-carousel-item
-        class="mt-16"
-        v-for="(itWords, index) in itWords"
-        :key="index"
-      >
-        <h1>{{ itWords.englishWord }} - {{ itWords.polishWord }}</h1>
-      </v-carousel-item>
-      
-    </v-carousel>
-
-  </v-card>
-      <v-card class="mx-auto mt-2" v-if="this.carouselCounter > 10 && this.testAgreement" color="green" width="500" height="130">
+  <v-container>
+    <v-card
+      v-if="(this.showQuestions = true)"
+      width="500"
+      height="300"
+      class="mx-auto mt-15"
+    >
+    {{showQuestions}}
+      {{ carouselCounter }}
+      <v-carousel hide-delimiters prev-icon @change="incrementCarouselCounter">
+        <v-carousel-item
+          class="mt-16"
+          v-for="(itWords, index) in itWords"
+          :key="index"
+        >
+          <h1>{{ itWords.englishWord }} - {{ itWords.polishWord }}</h1>
+        </v-carousel-item>
+      </v-carousel>
+    </v-card>
+    <v-card
+      class="mx-auto mt-2"
+      v-if="this.carouselCounter > 10 && this.testAgreement"
+      color="green"
+      width="500"
+      height="130"
+    >
       <h2>I think you've learned a lot of words</h2>
       <p>maybe you want to test your knowledge?</p>
-          <v-btn @click="agreeToTest" class="mr-2" color="green">yes</v-btn>
-          <v-btn @click="refuseTest" color="red">no</v-btn>
+      <v-btn @click="agreeToTest" class="mr-2" color="green">yes</v-btn>
+      <v-btn @click="refuseTest" color="red">no</v-btn>
     </v-card>
-</v-container>
-
+  </v-container>
 </template>
 
 <script>
@@ -31,14 +40,16 @@ export default {
       this.carouselCounter += 1;
     },
     agreeToTest() {
-        this.testAgreement = false
+      this.testAgreement = false;
+      this.showQuestions = !this.showQuestions
     },
     refuseTest() {
-                this.testAgreement = false
-    }
+      this.testAgreement = false;
+    },
   },
   data() {
     return {
+      showQuestions: true,
       testAgreement: true,
       askAboutTest: false,
       carouselCounter: 0,
