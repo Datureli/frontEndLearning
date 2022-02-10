a5b058403e941b09cb8c42b6958e0a9794436e62
Datureli <soleris1@gmail.com>
2022-02-10 15:54:31 +0100

    add remove category button on hover

--- src/components/RecruimentQuestions/FirstColumn.vue
+++ src/components/RecruimentQuestions/FirstColumn.vue
@@ -1,14 +1,32 @@
 <template>
   <v-card width="278" class="mt-10 ml-16" color="transparent" elevation="0">
     <v-list color="transparent">
-      <v-list-item-group  active-class="green--text">
+      <v-list-item-group active-class="green--text">
         <v-list-item
           v-for="(firstColumn, index) in firstColumn"
           :key="index"
           :to="firstColumn.link"
         >
           <v-icon>{{ firstColumn.icon }}</v-icon>
-          <v-list-item-title class="text-h6">{{ firstColumn.title }}</v-list-item-title>
+          <v-list-item-title class="text-h6">{{
+            firstColumn.title
+          }}</v-list-item-title>
+          <v-hover v-slot="{ hover }">
+            <v-card class="mx-auto" color="transparent" outlined max-width="600">
+              <v-expand-transition>
+                <div
+                  v-if="hover"
+                  class="d-flex transition-fast-in-fast-out darken-2 v-card--reveal text-h2 white--text"
+                >
+                  <v-btn icon>
+                    <v-icon>mdi-close</v-icon>
+                  </v-btn>
+                </div>
+              </v-expand-transition>
+
+              <v-card-text> </v-card-text>
+            </v-card>
+          </v-hover>
         </v-list-item>
       </v-list-item-group>
     </v-list>
@@ -18,8 +36,13 @@
 <script>
 import { mapActions, mapState } from "vuex";
 export default {
+  data() {
+    return {
+      showRmoveButton: false,
+    };
+  },
   computed: {
-    ...mapState('firstColumn',['firstColumn'])
+    ...mapState("firstColumn", ["firstColumn"]),
   },
   methods: {
     ...mapActions("questions", [
@@ -31,5 +54,16 @@ export default {
       "randomReactQuestion",
     ]),
   },
+  removeQuestionsCategory() {},
 };
 </script>
+<style>
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: 0.5;
+  position: absolute;
+  width: 100%;
+}
+</style>
