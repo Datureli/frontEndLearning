24ff4330e927006a2b66d2a998224a409885d62f
Datureli <soleris1@gmail.com>
2022-03-03 21:49:48 +0100

    rewrite music component and fixed overlimited size github bug

--- src/components/RecruimentQuestions/IconComponent.vue
+++ src/components/RecruimentQuestions/IconComponent.vue
@@ -3,33 +3,39 @@
     <v-icon
       large
       @click="globalRandomQuestion"
-      style="position: absolute; bottom: 20%; right:73%;"
+      style="position: absolute; bottom:3%; right:73%;"
       >mdi-dice-multiple</v-icon
     >
 
     <v-icon
       x-large
       @click="randomLoop"
-      style="position: absolute; bottom: 20%; right:70%;"
+      style="position: absolute; bottom:3%; right:70%;"
       >mdi-autorenew</v-icon
     >
 
     <v-icon
       large 
       @click="disableAnswers"
-      style="position: absolute; bottom: 20%; right:20%;"
+      style="position: absolute; bottom:3%; right:20%;"
       >mdi-comment-off-outline</v-icon
     >
       <v-icon
       x-large
       @click="randomLoop"
-      style="position: absolute; bottom: 20%; right:27%;"
+      style="position: absolute; bottom:3%; right:27%;"
       >mdi-music</v-icon
     >
+           <div class="controls">
+          <button class="prev" @click="prev">Prev</button>
+          <button class="play" v-if="!isPlaying" @click="play">Play</button>
+          <button class="pause" v-else @click="pause">Pause</button>
+          <button class="next" @click="next">Next</button>
+        </div>
     <v-tooltip bottom>
       <template v-slot:activator="{ on, attrs }">
         <v-btn
-          style="position: absolute; bottom: 20%; right:22%;"
+          style="position: absolute; bottom:3%; right:22%;"
           color="transparent"
           outlined
           v-bind="attrs"
@@ -54,12 +60,68 @@ export default {
   data() {
     return {
       disableLoop: false,
-    };
+       current: {},
+      index: 0,
+      isPlaying: false,
+      songs: [
+        {
+          title: 'Grateful',
+          artist: 'Neffex',
+          src: require('@/assets/drake.mp3')
+        },
+        {
+          title: 'Invincible',
+          artist: 'Deaf Kev',
+          src: require('@/assets/drake.mp3')
+        }
+      ],
+      player: new Audio()
+    }
+  },
+  created () {
+    this.current = this.songs[this.index];
+    this.player.src = this.current.src;
   },
   computed: {
     ...mapState(["disable", "favorite", "isHeartActive"]),
   },
   methods: {
+     play (song) {
+      if (typeof song.src != "undefined") {
+        this.current = song;
+        this.player.src = this.current.src;
+      }
+      this.player.play();
+      this.player.addEventListener('ended', function () {
+        this.index++;
+        if (this.index > this.songs.length - 1) {
+          this.index = 0;
+        }
+        this.current = this.songs[this.index];
+        this.play(this.current);
+      }.bind(this));
+      this.isPlaying = true;
+    },
+    pause () {
+      this.player.pause();
+      this.isPlaying = false;
+    },
+    next () {
+      this.index++;
+      if (this.index > this.songs.length - 1) {
+        this.index = 0;
+      }
+      this.current = this.songs[this.index];
+      this.play(this.current);
+    },
+    prev () {
+      this.index--;
+      if (this.index < 0) {
+        this.index = this.songs.length - 1;
+      }
+      this.current = this.songs[this.index];
+      this.play(this.current);
+    },
     globalRandomQuestion() {
       switch (this.$route.path) {
         case "/about/htmlquestions":
@@ -126,5 +188,5 @@ export default {
     ...mapActions("vueQuestions", ["vueQuestionLoop", "randomVueQuestion"]),
     ...mapActions(["disableAnswers", "addToFavorite"]),
   },
-};
+}
 </script>
