c7d57aafa150b3d6a04bcab6e084ab67eb0e9a3b
Datureli <soleris1@gmail.com>
2022-03-08 10:41:02 +0100

    add changeIcon function to disableComponent

--- src/components/Icons/DisableComponent.vue
+++ src/components/Icons/DisableComponent.vue
@@ -1,13 +1,38 @@
 <template>
   <v-btn color="transparent">
-    <v-icon x-large @click="disableAnswers">mdi-comment-off-outline</v-icon>
+    <v-icon
+      v-if="disableStatus"
+      x-large
+      @click="
+        disableAnswers();
+        changeIcon();
+      "
+      >mdi-comment-off-outline</v-icon
+    >
+    <v-icon
+      v-else
+      x-large
+      @click="
+        disableAnswers();
+        changeIcon();
+      "
+      >mdi-comment-outline</v-icon
+    >
   </v-btn>
 </template>
 
 <script>
 import { mapState, mapActions } from "vuex";
 export default {
+  data() {
+    return {
+      disableStatus: true,
+    };
+  },
   methods: {
+    changeIcon() {
+      this.disableStatus = !this.disableStatus;
+    },
     ...mapActions(["disableAnswers"]),
   },
   computed: {
