b3416b8739f59d3a54090d34b9bdd650068ee482
Datureli <soleris1@gmail.com>
2022-04-29 21:18:49 +0200

    add describeyourself component to english section

--- src/components/EnglishSection/English.vue
+++ src/components/EnglishSection/English.vue
@@ -1,6 +1,5 @@
 <template>
   <v-container fluid style="background: orange; height: 521px;">
-    dsa{{select}}
     <v-select
       v-model="select"
       class="pa-5 mt-0 mr-16 mx-auto"
@@ -13,12 +12,14 @@
       :to="englishSection.link"
       @change="select"
     ></v-select>
-    <ItWordsCollection v-if="select.name === 'angielski2'" />
+    <ItWordsCollection v-if="select.name === 'nauka słów'" />
+    <DescribeYourself v-if="select.name === 'opisz siebie'" />
   </v-container>
 </template>
 
 <script>
 import ItWordsCollection from "./ItWordsCollection.vue";
+import DescribeYourself from './DescribeYourself.vue'
 export default {
   
   data() {
@@ -28,16 +29,13 @@ export default {
 
       englishSection:  [
         {
-          name: 'angielski',
-          link: ''
+          name: 'opisz siebie',
         },
             {
-          name: 'angielski2',
-          link: '/words'
+          name: 'nauka słów',
         },
             {
           name: 'angielski3',
-          link: ''
         }
       ]
     }
@@ -45,6 +43,7 @@ export default {
   name: "Home",
   components: {
     ItWordsCollection,
+    DescribeYourself
   },
 }
 </script>
