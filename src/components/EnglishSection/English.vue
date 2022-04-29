e84f8e6ece8eaf0ad774f937efb9816d6baee480
Datureli <soleris1@gmail.com>
2022-04-29 18:11:49 +0200

    add english section array

--- src/components/EnglishSection/English.vue
+++ src/components/EnglishSection/English.vue
@@ -1,6 +1,15 @@
 <template>
   <v-container fluid style="background: orange; height: 521px;">
-    <v-select label="Standard"></v-select>
+    <v-select
+      v-model="select"
+      class="pa-5 mt-0 mr-16 mx-auto"
+      :items="englishSection"
+      item-text="name"
+      label="Choose category"
+      persistent-hint
+      return-object
+      single-line
+    ></v-select>
     <ItWordsCollection />
   </v-container>
 </template>
@@ -8,6 +17,24 @@
 <script>
 import ItWordsCollection from "./ItWordsCollection.vue";
 export default {
+  data() {
+    return {
+      englishSection:  [
+        {
+          name: 'angielski',
+          link: ''
+        },
+            {
+          name: 'angielski2',
+          link: ''
+        },
+            {
+          name: 'angielski3',
+          link: ''
+        }
+      ]
+    }
+  },
   name: "Home",
   components: {
     ItWordsCollection,
