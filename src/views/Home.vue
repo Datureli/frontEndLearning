87f5beaca3ea9a44a900bca21ae7f911f1c4f71b
Datureli <soleris1@gmail.com>
2022-06-04 13:40:00 +0200

    fix - remove height switch steatment in home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,6 @@
   <v-container-fluid>
     <v-lazy>
       <v-img
-        contain
-        :height="height"
         max-width="500"
         class="mx-auto"
         src="../assets/lightbulb.jpg"
@@ -11,27 +9,6 @@
       </v-img>
     </v-lazy>
 
-    <h1 class="white--text">First polish front-end learning platform</h1>
+    <h1 class="white--text mt-15">First polish front-end learning platform</h1>
   </v-container-fluid>
 </template>
-
-<script>
-export default {
-  computed: {
-    height() {
-      switch (this.$vuetify.breakpoint.name) {
-        case "xs":
-          return 520;
-        case "sm":
-          return 350;
-        case "md":
-          return 400;
-        case "lg":
-          return 380;
-        case "xl":
-          return 400;
-      }
-    },
-  },
-};
-</script>
