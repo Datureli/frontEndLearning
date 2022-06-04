dbb06e5488f94f6f632ce9a15b3adabdde608680
Datureli <soleris1@gmail.com>
2022-06-04 02:11:56 +0200

    perf - shorthand v-if and v-else with ternary operator

--- src/components/Icons/DisableComponent.vue
+++ src/components/Icons/DisableComponent.vue
@@ -1,18 +1,16 @@
 <template>
   <v-btn
     color="transparent"
-    @click="
-      disableAnswers();
-      changeIcon();
-    "
+    @click="disableAnswers(); changeIcon();"
   >
-    <v-icon v-if="disableStatus" x-large>mdi-comment-off-outline</v-icon>
-    <v-icon v-else x-large>mdi-comment-outline</v-icon>
+    <v-icon x-large>{{
+      disableStatus ? "mdi-comment-off-outline" : "mdi-comment-outline"
+    }}</v-icon>
   </v-btn>
 </template>
 
 <script>
-import { mapState, mapActions } from "vuex";
+import { mapActions } from "vuex";
 export default {
   data() {
     return {
@@ -24,9 +22,6 @@ export default {
       this.disableStatus = !this.disableStatus;
     },
     ...mapActions(["disableAnswers"]),
-  },
-  computed: {
-    ...mapState(["disable"]),
   },
 };
 </script>
