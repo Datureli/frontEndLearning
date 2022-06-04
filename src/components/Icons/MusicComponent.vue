c5b671db3dd41a431956427f863adc1972bca6ab
Datureli <soleris1@gmail.com>
2022-06-04 03:07:52 +0200

    refactor musicComponent with vue class

--- src/components/Icons/MusicComponent.vue
+++ src/components/Icons/MusicComponent.vue
@@ -8,80 +8,74 @@
 </template>
 
 <script>
-import Component from 'vue-class-component'
+import Vue from "vue";
+import Component from "vue-class-component";
 
 @Component
 export default class Counter extends Vue {
+  current = {};
+  index = 0;
+  isPlaying = false;
+  songs = [
+    {
+      title: "Grateful",
+      artist: "Neffex",
+      src: require("@/assets/drake.mp3"),
+    },
+    {
+      title: "Invincible",
+      artist: "Deaf Kev",
+      src: require("@/assets/hack.mp3"),
+    },
+  ];
+  player = new Audio();
 
-}
-export default {
-  data() {
-    return {
-      current: {},
-      index: 0,
-      isPlaying: false,
-      songs: [
-        {
-          title: "Grateful",
-          artist: "Neffex",
-          src: require("@/assets/drake.mp3"),
-        },
-        {
-          title: "Invincible",
-          artist: "Deaf Kev",
-          src: require("@/assets/hack.mp3"),
-        },
-      ],
-      player: new Audio(),
-    };
-  },
   created() {
     this.current = this.songs[this.index];
     this.player.src = this.current.src;
-  },
-  methods: {
-    play(song) {
-      if (typeof song.src != "undefined") {
-        this.current = song;
-        this.player.src = this.current.src;
-      }
-      this.player.play();
-      this.player.addEventListener(
-        "ended",
-        function() {
-          this.index++;
-          if (this.index > this.songs.length - 1) {
-            this.index = 0;
-          }
-          this.current = this.songs[this.index];
-          this.play(this.current);
-        }.bind(this)
-      );
-      this.isPlaying = true;
-    },
-    pause() {
-      this.player.pause();
-      this.isPlaying = false;
-    },
-    next() {
-      this.index++;
-      if (this.index > this.songs.length - 1) {
-        this.index = 0;
-      }
-      this.current = this.songs[this.index];
-      this.play(this.current);
-    },
-    prev() {
-      let index = this.index
-      let songs = this.songs
-      let current = this.current
-      index--;
-      if (index < 0) {
-        index = songs.length - 1;
-      }
-      current = songs[index];
-      this.play(current);
-    },
-  },
-};
+  }
+
+  play(song) {
+    if (typeof song.src != "undefined") {
+      this.current = song;
+      this.player.src = this.current.src;
+    }
+    this.player.play();
+    this.player.addEventListener(
+      "ended",
+      function() {
+        this.index++;
+        if (this.index > this.songs.length - 1) {
+          this.index = 0;
+        }
+        this.current = this.songs[this.index];
+        this.play(this.current);
+      }.bind(this)
+    );
+    this.isPlaying = true;
+  }
+  pause() {
+    this.player.pause();
+    this.isPlaying = false;
+  }
+  next() {
+    this.index++;
+    if (this.index > this.songs.length - 1) {
+      this.index = 0;
+    }
+    this.current = this.songs[this.index];
+    this.play(this.current);
+  }
+  prev() {
+    let index = this.index;
+    let songs = this.songs;
+    let current = this.current;
+    index--;
+    if (index < 0) {
+      index = songs.length - 1;
+    }
+    current = songs[index];
+    this.play(current);
+  }
+}
 </script>
