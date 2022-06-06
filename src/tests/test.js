ad92c319e1d2424b7de61b6da0328370ff5ffeda
Datureli <soleris1@gmail.com>
2022-06-06 14:16:51 +0200

    tests- add test for navigation icon

--- src/tests/test.js
+++ src/tests/test.js
@@ -1,23 +1,22 @@
 const { By, Key, Builder } = require("selenium-webdriver");
 require("chromedriver");
 
-  async function example() {
-    var searchString = "Automation testing with Selenium and JavaScript";
+async function example() {
+  var searchString = "Automation testing with Selenium and JavaScript";
 
-    //To wait for browser to build and launch properly
-    let driver = await new Builder().forBrowser("chrome").build();
+  let driver = await new Builder().forBrowser("chrome").build();
 
-    //To fetch http://google.com from the browser with our code.
-    await driver.get("http://google.com");
+  //To fetch http://google.com from the browser with our code.
+  await driver.get("https://cranky-leakey-58c4cc.netlify.app/");
 
-    //To send a search query by passing the value in searchString.
-    await driver.findElement(By.name("q")).sendKeys(searchString, Key.RETURN);
+  //To send a search query by passing the value in searchString.
+  let element = await driver.findElement(By.id('test'))
+  element.click()
+  //Verify the page title and print it
+  var title = await driver.getTitle();
+  console.log("Title is:", title);
 
-    //Verify the page title and print it
-    var title = await driver.getTitle();
-    console.log("Title is:", title);
-
-    //It is always a safe practice to quit the browser after execution
-    await driver.quit();
-  }
-example()
+  //It is always a safe practice to quit the browser after execution
+ // await driver.quit();
+}
+example();
