1245caeaa5519215c5dff46c02a1c4c4c15ffda1
Datureli <soleris1@gmail.com>
2022-06-07 17:32:26 +0200

    get rid of useless comments

--- tests/test.js
+++ tests/test.js
@@ -6,13 +6,11 @@ async function example() {
 
   let driver = await new Builder().forBrowser("chrome").build();
 
-  //To fetch http://google.com from the browser with our code.
   await driver.get("https://cranky-leakey-58c4cc.netlify.app/");
 
-  //To send a search query by passing the value in searchString.
   let element = await driver.findElement(By.id('test'))
   element.click()
-  //Verify the page title and print it
+
   var title = await driver.getTitle();
   console.log("Title is:", title);
 
