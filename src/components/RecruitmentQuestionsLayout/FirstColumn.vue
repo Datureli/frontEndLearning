b5983fbb564b36dddcd3a2c4e72f386bb49b08c8
Datureli <soleris1@gmail.com>
2022-08-01 09:43:37 +0200

    add dialog function after remove category

--- src/components/RecruitmentQuestionsLayout/FirstColumn.vue
+++ src/components/RecruitmentQuestionsLayout/FirstColumn.vue
@@ -8,16 +8,45 @@
           :to="firstColumn.link"
         >
           <v-icon>{{ firstColumn.icon }}</v-icon>
-          <p
-            style="width: 300px;"
-            class="text-h5 mx-auto justify-center"
-            v-if="firstColumn.title == null"
-          >
-            empty slot
-          </p>
+
           <v-list-item-title class="ml-5">{{
             firstColumn.title
           }}</v-list-item-title>
+          <div class="text-center" v-if="firstColumn.title == null">
+            <v-dialog v-model="dialog" width="500" persistent>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn color="green lighten-2" dark v-bind="attrs" v-on="on">
+                  empty slot
+                </v-btn>
+              </template>
+
+              <v-card>
+                <v-card-title class="text-h5 grey lighten-2">
+                  Wybierz kategorię
+                </v-card-title>
+
+                <v-card-text>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </v-card-text>
+
+                <v-divider></v-divider>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="primary" text @click="dialog = false">
+                    I accept
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </div>
 
           <v-hover v-slot="{ hover }">
             <v-card
@@ -51,6 +80,11 @@
 <script>
 import { mapActions, mapState } from "vuex";
 export default {
+  data() {
+    return {
+      dialog: false,
+    };
+  },
   computed: {
     ...mapState("firstColumn", ["firstColumn"]),
     // eslint-disable-next-line vue/return-in-computed-property
