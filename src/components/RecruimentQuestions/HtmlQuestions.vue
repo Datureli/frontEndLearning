0500c42dff3d63f5b32cc8f0fae1c947d4f7648b
Datureli <soleris1@gmail.com>
2022-08-23 19:51:40 +0200

    fix html questions

---
+++ src/components/RecruimentQuestions/HtmlQuestions.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-carousel hide-delimiters>
+    <v-carousel-item
+      v-for="(htmlQuestions, index) in htmlQuestions"
+      :key="index"
+      max-width="700"
+      class="mx-auto"
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-subtitle
+            class="text--primary font-weight-black text-h4 text-wrap "
+            v-text="htmlQuestions.question"
+          ></v-list-item-subtitle>
+
+          <v-list-item-subtitle
+            class="white--text text-justify text-h6 text-wrap"
+            v-show="disable"
+            v-text="htmlQuestions.answer"
+          ></v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  computed: {
+    ...mapState(["disable"]),
+    ...mapState("htmlQuestions", ["htmlQuestions"]),
+  },
+};
+</script>
