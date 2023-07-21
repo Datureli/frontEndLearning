cedbfcab4fdd7e2fd74cc388e8d2f01f0d4b4952
Datureli <soleris1@gmail.com>
2023-07-21 13:13:20 +0200

    fix layout of intro to course

--- src/components/courses/HtmlCourse.vue
+++ src/components/courses/HtmlCourse.vue
@@ -1,57 +1,71 @@
 <template>
-  <v-container fluid class="darkGradient full-height" style="height: 100vh">
-    <v-row class="mt-15">
-      <v-col cols="3">
-     <img width="600" height="300" src="../../assets/it.webp" alt="Course Image" />
+  <v-container fluid class="module full-height" style="height: 100vh">
+    <v-row class="mt-12 ">
+      <v-col    >
+        <v-card class="module">
+          <img
+            width="600"
+            height="300"
+            src="../../assets/it.webp"
+            alt="Course Image"
+          />
+          <h1 class="mt-3">Kurs podstaw html</h1>
+        </v-card>
       </v-col>
-      <v-col cols="9">
-        <h1>Kurs podstaw html</h1>
-        <p class="text-left">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
+      <v-col    >
+        <p class="text-left white--text font-weight-bold">
+          Kurs podstaw HTML to wprowadzenie do języka znaczników używanego do
+          tworzenia stron internetowych. W tym kursie uczestnicy zdobędą wiedzę
+          na temat różnych elementów HTML i nauczą się, jak budować strony
+          internetowe od podstaw. Omówione zostaną podstawowe znaczniki, takie
+          jak nagłówki, akapity, listy, linki i obrazy, a także wykorzystanie
+          ich do organizowania treści na stronie. Kurs skupi się również na
+          tworzeniu poprawnej struktury HTML oraz zasadach semantyki, które
+          pomogą uczestnikom zrozumieć znaczenie i cel użytych elementów.
         </p>
+        <v-dialog
+          v-model="dialog"
+          fullscreen
+          @keydown.esc="dialog = false"
+          transition="dialog-bottom-transition"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn width="100%" class="custom-styled-button font-weight-bold" color="#ff5722" dark v-bind="attrs" v-on="on">
+              Zacznij naukę
+            </v-btn>
+          </template>
+          <v-card color="darkGradient">
+            <v-btn
+              x-large
+              style="right: 1%"
+              icon
+              dark
+              @click="closeModal"
+              absolute
+            >
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+          </v-card>
+        </v-dialog>
       </v-col>
     </v-row>
-
-    <v-dialog
-      v-model="dialog"
-      fullscreen
-      @keydown.esc="dialog = false"
-      transition="dialog-bottom-transition"
-    >
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn id="login" color="transparent" dark v-bind="attrs" v-on="on">
-          Zacznij naukę
-        </v-btn>
-      </template>
-      <v-card color="darkGradient">
-        <v-btn x-large style="right: 1%" icon dark @click="closeModal" absolute>
-          <v-icon>mdi-close</v-icon>
-        </v-btn>
-      </v-card>
-    </v-dialog>
   </v-container>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      items: ["Opcja 1", "Opcja 2", "Opcja 3", "opcja 4", "opcja5"],
-    };
-  },
-  methods: {
-    selectItem(item) {
-      // Obsługa wyboru elementu z listy
-      console.log("Wybrano element:", item);
-    },
-  },
-};
-</script>
+<style scoped>
+.module {
+  background: #222;
+  height: 420px;
+  color: white;
+  padding: 2rem;
+}
+  .custom-styled-button {
+    /* Dodaj dowolne style, które chcesz zastosować dla przycisku */
+    /* Przykładowe style: */
+    background-color: #ff5722; /* Kolor tła przycisku */
+    color: #ffffff; /* Kolor tekstu na przycisku */
+    border-radius: 8px; /* Zaokrąglenie rogów przycisku */
+    padding: 10px 20px; /* Rozmiar wewnętrznego marginesu przycisku */
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2); /* Cień przycisku */
+  }
+</style>
---
+++ src/components/courses/IntroToCourse.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-card class="module">
+    <slot>
+      <v-container fluid class="darkGradient full-height" style="height: 100vh">
+        <v-row class="mt-15">
+          <v-col cols="6">
+            <img
+              width="600"
+              height="300"
+              src="../../assets/it.webp"
+              alt="Course Image"
+            />
+          </v-col>
+          <v-col cols="6">
+            <h1>Kurs podstaw html</h1>
+            <p class="text-left">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </p>
+            <v-dialog
+              v-model="dialog"
+              fullscreen
+              @keydown.esc="dialog = false"
+              transition="dialog-bottom-transition"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  id="login"
+                  color="transparent"
+                  dark
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  Zacznij naukę
+                </v-btn>
+              </template>
+              <v-card color="darkGradient">
+                <v-btn
+                  x-large
+                  style="right: 1%"
+                  icon
+                  dark
+                  @click="closeModal"
+                  absolute
+                >
+                  <v-icon>mdi-close</v-icon>
+                </v-btn>
+              </v-card>
+            </v-dialog>
+          </v-col>
+        </v-row>
+      </v-container>
+    </slot>
+  </v-card>
+</template>
+
+<style scoped>
+.module-border-wrap {
+  max-width: 250px;
+  padding: 1rem;
+
+  position: relative;
+  background: linear-gradient(to right, red, purple);
+  padding: 3px;
+}
+
+.module {
+  background: #222;
+  height: 100%;
+  color: white;
+  padding: 2rem;
+}
+</style>
