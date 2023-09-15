87b862dcaaf44ef1a5418191222fadba2045b9bd
Datureli <soleris1@gmail.com>
2023-09-15 21:34:53 +0200

    remove unused section

--- src/store/modules/cyberSecurityQuestions.js
+++ src/store/modules/cyberSecurityQuestions.js
@@ -9,19 +9,19 @@ export default {
             "Hack The Box (HTB) to platforma treningowa i społeczność skupiająca się na testowaniu penetracyjnym (penetration testing) oraz bezpieczeństwie informatycznym. Jest to platforma online, która umożliwia użytkownikom doskonalenie swoich umiejętności związanych z hackingiem etycznym poprzez praktyczne wyzwania i zadania.Wyzwania i maszyny: Hack The Box oferuje różnorodne wyzwania i maszyny, które można próbować włamać. Te maszyny są wirtualnymi środowiskami, które symulują realistyczne scenariusze ataków i pozwalają użytkownikom testować swoje umiejętności w bezpiecznym środowisku.",
         },
         {
-            question: "filtry vs akcje?",
+            question: "Czym jest wifi?",
             answer:
-              "Filtr to funkcja, która modyfikuje otrzymane dane, natomiast akcja to funkcja, która jest wywoływana w odpowiednim momencie.Zatem filtrów będziemy używać wtedy, gdy na przykład chcemy zmodyfikować istniejący kod HTML, natomiast akcji użyjemy do dodania nieistniejącego jeszcze kodu HTML.",
+              "Wi-Fi, czyli Wireless Fidelity, to technologia komunikacji bezprzewodowej, która umożliwia urządzeniom komunikację i wymianę danych przez fale radiowe. Działa na zasadzie wykorzystania częstotliwości radiowych w celu przesyłania informacji między urządzeniami w sieci lokalnej. Oto ogólny opis działania technologii Wi-FiUrządzenie, które chce wysłać lub odbierać dane za pomocą Wi-Fi, generuje sygnał elektryczny, który jest przekształcany na fale radiowe o odpowiednich częstotliwościach. Te fale radiowe są wytwarzane w zakresie częstotliwości radiowych lub mikrofalowych, co pozwala na efektywną transmisję danych na krótkie odległości.",
           },
           {
-            question: "filtry vs akcje?",
+            question: "Czym jest nfc?",
             answer:
-              "Filtr to funkcja, która modyfikuje otrzymane dane, natomiast akcja to funkcja, która jest wywoływana w odpowiednim momencie.Zatem filtrów będziemy używać wtedy, gdy na przykład chcemy zmodyfikować istniejący kod HTML, natomiast akcji użyjemy do dodania nieistniejącego jeszcze kodu HTML.",
+              "NFC, czyli Near Field Communication (Komunikacja Bliskiego Zasięgu), to technologia bezprzewodowej komunikacji krótkiego zasięgu, która umożliwia wymianę danych między urządzeniami znajdującymi się w bliskiej odległości od siebie, zazwyczaj na odległość do kilku centymetrów. Technologia NFC bazuje na standardach radiowych RFID (Radio Frequency Identification) i pozwala na komunikację między dwoma urządzeniami, z których przynajmniej jedno jest aktywne (wysyła dane), a drugie może być aktywne lub pasywne (odbiera dane).",
           },
           {
-            question: "Opisz funkcję have_posts",
+            question: "Czym jest OS-Independent Troubleshooting",
             answer:
-              "Określa, czy w bieżącym zapytaniu WordPress są posty w pętli",
+              "OS-Independent Troubleshooting oznacza proces diagnozowania i rozwiązywania problemów technicznych związanych z urządzeniem lub oprogramowaniem, niezależnie od systemu operacyjnego (OS), na którym działa to urządzenie lub oprogramowanie. Innymi słowy, jest to podejście do rozwiązywania problemów, które skupia się na ogólnych aspektach i cechach, niezależnie od konkretnej platformy lub systemu operacyjnego.",
           },
           {
             question: "Opisz funkcję is_single",
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,18 @@
 <template>
-  <v-container fluid>
-    <v-parallax height="500" class="mx-auto" src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+  <v-container fluid >
+    <v-parallax
+      class="mx-auto"
+      src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+      style="height: 100vh;"
+    >
       <h1 class="white--text mt-15">
         First polish front-end learning platform
       </h1>
     </v-parallax>
-
-    <v-container class="text-container mx-auto" fluid>
-      <v-row class="flex-md-row flex-column-reverse ml-md-12" >
-        <v-col cols="12" md="6">
-          <p class="mt-5 text-left white--text mx-5 ">
-            W tej poprawionej wersji kodu użyliśmy systemu siatek Vuetify, aby
-            uczynić karty responsywne i dostosować ich układ w zależności od
-            rozmiaru ekranu. Zastosowaliśmy pola cols na komponencie v-col do
-            określenia liczby kolumn, które karta będzie zajmować na różnych
-            rozmiarach ekranu. Na przykład cols="12" md="8" lg="6" oznacza, że
-            karta zajmie pełną szerokość na małych ekranach, 8 kolumn na ekranach
-            o średnim rozmiarze i 6 kolumn na dużych ekranach.
-          </p>
-        </v-col>
-        <v-col cols="12" md="6">
-          <div>
-            <h1 class="white--text mt-15">Second Section</h1>
-            <p>twój kod aktywacyjny</p>
-          </div>
-        </v-col>
-      </v-row>
-    </v-container>
   </v-container>
 </template>
 
 <style>
-.container {
-  padding: 0;
-}
 .text-container {
   background-color: rgb(27, 100, 190);
   position: relative;
