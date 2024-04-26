affce7e6dde4ce0547bec17be558aa206ae2b049
Datureli <soleris1@gmail.com>
2024-04-26 21:26:05 +0200

    improve router file by generating question category loop

--- src/router/index.js
+++ src/router/index.js
@@ -16,163 +16,40 @@ const routes = [
     path: "/recruitment-questions/",
     name: "RecruitmentQuestions",
     component: () => import("../views/RecruitmentQuestions.vue"),
-    children: [
-      {
-        path: "cssquestions",
-        component: () =>
-          import("../components/RecruimentQuestions/RecruitmentQuestion.vue"),
-        name: "RecruitmentQuestion",
-      },
-      {
-        path: "javascript",
-        component: () =>
-          import("../components/RecruimentQuestions/RecruitmentQuestion.vue"),
-        name: "JavascriptQuestions",
-      },
-      {
-        path: "vue",
-        component: () =>
-          import("../components/RecruimentQuestions/RecruitmentQuestion.vue"),
-        name: "VueQuestions",
-      },
-      {
-        path: "react",
-        component: () =>
-          import("../components/RecruimentQuestions/RecruitmentQuestion.vue"),
-        name: "ReactQuestions",
-      },
-      {
-        path: "test",
-        component: () =>
-          import("../components/RecruimentQuestions/RecruitmentQuestion.vue"),
-        name: "TestQuestions",
-      },
-      {
-        path: "typescript",
-        component: () =>
-          import("../components/RecruimentQuestions/RecruitmentQuestion.vue"),
-        name: "TypeScriptQuestions",
-      },
-      {
-        path: "general",
-        component: () =>
-          import("../components/RecruimentQuestions/RecruitmentQuestion.vue"),
-        name: "GeneralQuestions",
-      },
-
-      {
-        path: "htmlquestions",
-        component: () =>
-          import("../components/RecruimentQuestions/RecruitmentQuestion.vue"),
-        name: "HtmlQuestions",
-      },
-      {
-        path: "gitquestions",
-        component: () =>
-          import("../components/RecruimentQuestions/RecruitmentQuestion.vue"),
-        name: "GitQuestions",
-      },
-      {
-        path: "test-knowledge",
-        component: () =>
-          import("../components/RecruimentQuestions/TestYourKnowledge"),
-        name: "TestYourKnowledge",
-      },
-      {
-        path: "cypress",
-        component: () =>
-          import("../components/RecruimentQuestions/RecruitmentQuestion.vue"),
-        name: "Cypress",
-      },
-      {
-        path: "postman",
-        component: () =>
-          import("../components/RecruimentQuestions/RecruitmentQuestion.vue"),
-        name: "Postman",
-      },
-      {
-        path: "sql",
-        component: () =>
-          import("../components/RecruimentQuestions/RecruitmentQuestion.vue"),
-        name: "Sql",
-      },
-      {
-        path: "wordpress",
-        component: () =>
-          import("../components/RecruimentQuestions/RecruitmentQuestion.vue"),
-        name: "Wordpress",
-      },
-      {
-        path: "cybersecurity",
-        component: () =>
-          import("../components/RecruimentQuestions/RecruitmentQuestion.vue"),
-        name: "Cybersecurity",
-      },
-      {
-        path: "cloud",
-        component: () =>
-          import("../components/RecruimentQuestions/RecruitmentQuestion.vue"),
-        name: "Cloud",
-      },
-      {
-        path: "cicd",
-        component: () =>
-          import("../components/RecruimentQuestions/RecruitmentQuestion.vue"),
-        name: "Cicd",
-      },
-    ],
+    children: generateRecruitmentQuestionRoutes([
+      "htmlquestions",
+      "cssquestions",
+      "javascript",
+      "vue",
+      "react",
+      "test",
+      "typescript",
+      "general",
+      "gitquestions",
+      "test-knowledge",
+      "cypress",
+      "postman",
+      "sql",
+      "wordpress",
+      "cybersecurity",
+      "cloud",
+      "cicd",
+    ]),
   },
-
   {
     path: "/book/",
     name: "Book",
     component: () => import("../components/materials/Books/Book"),
     children: [
-      {
-        path: "html",
-        name: "Html",
-        component: () => import("../components/materials/Books/HtmlBook"),
-      },
-      {
-        path: "css",
-        name: "Css",
-        component: () => import("../components/materials/Books/CssBook"),
-      },
-      {
-        path: "javascript",
-        name: "Javascript",
-        component: () => import("../components/materials/Books/JavascriptBook"),
-      },
-      {
-        path: "vue",
-        name: "Vue",
-        component: () => import("../components/materials/Books/VueBook"),
-      },
-      {
-        path: "react",
-        name: "React",
-        component: () => import("../components/materials/Books/ReactBook"),
-      },
-      {
-        path: "typescript",
-        name: "Typescript",
-        component: () => import("../components/materials/Books/TypescriptBook"),
-      },
-      {
-        path: "git",
-        name: "Git",
-        component: () => import("../components/materials/Books/GitBook"),
-      },
-      {
-        path: "general",
-        name: "General",
-        component: () => import("../components/materials/Books/GeneralBook"),
-      },
-      {
-        path: "tests",
-        name: "Tests",
-        component: () => import("../components/materials/Books/TestBook"),
-      },
+      { path: "html", name: "Html", component: () => import("../components/materials/Books/HtmlBook") },
+      { path: "css", name: "Css", component: () => import("../components/materials/Books/CssBook") },
+      { path: "javascript", name: "Javascript", component: () => import("../components/materials/Books/JavascriptBook") },
+      { path: "vue", name: "Vue", component: () => import("../components/materials/Books/VueBook") },
+      { path: "react", name: "React", component: () => import("../components/materials/Books/ReactBook") },
+      { path: "typescript", name: "Typescript", component: () => import("../components/materials/Books/TypescriptBook") },
+      { path: "git", name: "Git", component: () => import("../components/materials/Books/GitBook") },
+      { path: "general", name: "General", component: () => import("../components/materials/Books/GeneralBook") },
+      { path: "tests", name: "Tests", component: () => import("../components/materials/Books/TestBook") },
     ],
   },
   {
@@ -213,3 +90,15 @@ router.beforeEach((to, from, next) => {
 });
 
 export default router;
+
+function generateRecruitmentQuestionRoutes(categories) {
+  return categories.map(category => ({
+    path: category,
+    component: () => import("../components/RecruimentQuestions/RecruitmentQuestion.vue"),
+    name: capitalizeFirstLetter(category) + "Questions",
+  }));
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
